1a56513f1c5a10ccd35e151e35c1dc5b9ccc24f7
apophis9283 <steaven.android@gmail.com>
2021-02-24 11:24:44 -0500

    Remove lineage and trust

--- common.mk
+++ common.mk
@@ -276,8 +276,7 @@ EXTRA_FOD_ANIMATIONS := true
 
 # Prebuilts
 PRODUCT_PACKAGES += \
-    Adaway \
-    Sounds
+    Adaway
 
 # Fingerprint
 PRODUCT_PACKAGES += \
@@ -335,10 +334,6 @@ PRODUCT_PACKAGES += \
     IPACM_cfg.xml \
     libipanat \
     liboffloadhal
-
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.oneplus_kona
 
 # Media
 PRODUCT_COPY_FILES += \
@@ -467,10 +462,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     KeyHandler \
     tri-state-key_daemon
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # Update engine
 PRODUCT_PACKAGES += \
